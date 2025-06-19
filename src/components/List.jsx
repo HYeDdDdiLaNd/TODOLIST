5f31c94f9628dc05af02bf547f2ff6a36658930d
HYeDdDiLaNd <anotherHyedddi@gmail.com>
2025-06-19 20:39:45 +0900

    refactor: 할 일 카운트에 대한 useMemo 사용으로 불필요한 연산 최적화

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import TodoItem from '../components/TodoItem';
 
 const List = ({ todoData, onChangeChecked, onRemoveTodo }) => {
@@ -21,8 +21,41 @@ const List = ({ todoData, onChangeChecked, onRemoveTodo }) => {
 
     return searchMatch && completeTodoMatch; //return값은 todo 개별요소의 판단이고, 모두 true여야 true를 반환
   });
-  const completeTodoLength = todoData.filter((todo) => todo.isChecked).length;
-  const incCompleteTodoLength = todoData.length - completeTodoLength;
+
+  const { totalTodoLength, completeTodoLength, incCompleteTodoLength } =
+    useMemo(() => {
+      //딱 한번만 수행
+      //3. 연산의 값을 반환해주기 때문에 그 값을 변수에 담아서 받을 수 있고, 구조 분해 할당도 가능하다.
+
+      //1. 메모이제이션 하고 싶은 연산을 콜백으로 넣으면
+      console.log('몇번 출력하냐');
+      const totalTodoLength = todoData.length;
+      const completeTodoLength = todoData.filter(
+        (todo) => todo.isChecked
+      ).length;
+      const incCompleteTodoLength = totalTodoLength - completeTodoLength;
+
+      return {
+        totalTodoLength,
+        completeTodoLength,
+        incCompleteTodoLength, //2. 그 연산 값을 반화해준다.
+      };
+    }, [todoData]); //의존성 배열, 배열이 바뀌면 콜백을 실행, ; todoData가 바뀌면 실행.
+
+  // const getAnalyzedData = () => {  이렇게 하면 리스트 내에서 검색창이 활성화 될 떄에도 불필요한 연산을 실행함. 그래서 useMemo 를 사용ㅇ하고
+  //   const totalTodoLength = todoData.length;
+  //   const completeTodoLength = todoData.filter((todo) => todo.isChecked).length;
+  //   const incCompleteTodoLength = totalTodoLength - completeTodoLength;
+
+  //   return {
+  //     totalTodoLength,
+  //     completeTodoLength,
+  //     incCompleteTodoLength,
+  //   };
+  // };
+
+  // const { totalTodoLength, completeTodoLength, incCompleteTodoLength } =
+  //   getAnalyzedData();
 
   return (
     <div className="list">
@@ -35,6 +68,10 @@ const List = ({ todoData, onChangeChecked, onRemoveTodo }) => {
         onChange={onChangeSearched}
       />
       <div className="sort">
+        <dl>
+          <dt>총 할 일</dt>
+          <dd>{totalTodoLength}개</dd>
+        </dl>
         <dl>
           <dt>완료된 할 일</dt>
           <dd>{completeTodoLength}개</dd>
